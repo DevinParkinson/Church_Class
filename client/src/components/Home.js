8e65caca94e1bb39fc0cf3d0a5727946b1ab6b18
Devin Parkinson <dparky13@icloud.com>
2019-01-07 23:08:35 -0700

    just adding a little color

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,9 @@ import { Header } from 'semantic-ui-react';
 class Home extends Component {
   render() {
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
+      <div style={{backgroundColor: "blue", height: "100vh", color: "white"}}>
+        <Header as='h1' textAlign='center' style={{color: "white"}}>Hello 17 year olds!</Header>
+      </div>
     );
   }
 }
