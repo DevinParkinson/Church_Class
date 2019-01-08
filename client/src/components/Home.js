6461c7b39853bb078e5f14242fbb2239e12f1f04
Devin Parkinson <dparky13@icloud.com>
2019-01-07 23:13:14 -0700

    adding textloop on title

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
+import TextLoop from "react-text-loop"
 
 class Home extends Component {
   render() {
     return (
       <div style={{backgroundColor: "blue", height: "100vh", color: "white"}}>
-        <Header as='h1' textAlign='center' style={{color: "white"}}>Hello 17 year olds!</Header>
+        <TextLoop style={{textAlign: "center"}}>
+          <Header as='h1' style={{color: "white"}}>Hello 17 year olds!</Header>
+          <Header as='h1' style={{color: "white"}}>Welcome to Class today!</Header>
+          <Header as='h1' style={{color: "white"}}>Let's Rock this!!!</Header>
+        </TextLoop>
       </div>
     );
   }
