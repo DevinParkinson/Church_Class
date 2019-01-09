6da7c31888ecf00bbf6b1d05672301dae517a91a
Devin Parkinson <dparky13@icloud.com>
2019-01-08 20:34:42 -0700

    image as 100%

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ import TextLoop from "react-text-loop"
 class Home extends Component {
   render() {
     return (
-      <div style={{ backgroundImage: "url(https://www.followmeretreat.org/wp-content/uploads/2017/03/cropped-Jesus-Hand-Vanilla-2.jpg)", backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: "70vh", backgroundColor: "white", backgroundPosition: "center", position: "relative" }}>
+      <div style={{ backgroundImage: "url(https://www.followmeretreat.org/wp-content/uploads/2017/03/cropped-Jesus-Hand-Vanilla-2.jpg)", backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: "100vh", backgroundColor: "white", backgroundPosition: "center", position: "relative" }}>
         <div style={{textAlign: "center", position: "absolute", top: "25%", left: "40%", color: "white"}}>
           <TextLoop>
             <Header as='h1' style={{color: "white"}}>Hello 17 year olds!</Header>
