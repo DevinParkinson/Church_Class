c861efab112ddd457ca34d9844724c70d03d1474
Devin Parkinson <dparky13@icloud.com>
2019-01-09 20:16:40 -0700

    adding week1 card

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Grid, Card, Divider } from 'semantic-ui-react';
 import TextLoop from "react-text-loop"
 
 class Home extends Component {
   render() {
     return (
-      <div style={{ backgroundImage: "url(https://www.followmeretreat.org/wp-content/uploads/2017/03/cropped-Jesus-Hand-Vanilla-2.jpg)", backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: "100vh", backgroundColor: "white", backgroundPosition: "center", position: "relative" }}>
-        <div style={{textAlign: "center", position: "absolute", top: "25%", left: "40%", color: "white"}}>
-          <TextLoop>
-            <Header as='h1' style={{color: "white"}}>Hello 17 year olds!</Header>
-            <Header as='h1' style={{color: "white"}}>Welcome to your own website for this year!</Header>
-            <Header as='h1' style={{color: "white"}}>Let's Rock this!!!</Header>
-          </TextLoop>
+      <div>
+        <div style={{ backgroundImage: "url(https://www.followmeretreat.org/wp-content/uploads/2017/03/cropped-Jesus-Hand-Vanilla-2.jpg)", backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: "100vh", backgroundColor: "white", backgroundPosition: "center", position: "relative" }}>
+          <div style={{textAlign: "center", position: "absolute", top: "25%", left: "40%", color: "white"}}>
+            <TextLoop>
+              <Header as='h1' style={{color: "white"}}>Hello 17 year olds!</Header>
+              <Header as='h1' style={{color: "white"}}>Welcome to your own website for this year!</Header>
+              <Header as='h1' style={{color: "white"}}>Let's Rock this!!!</Header>
+            </TextLoop>
+          </div>
+          <Header as='h2' style={{textAlign: "center", position: "absolute", top: "30%", left: "35%", color: "white"}}>To Get Started, Click on the current week below.</Header>
+          <Divider />
         </div>
-        <Header as='h2' style={{textAlign: "center", position: "absolute", top: "30%", left: "35%", color: "white"}}>To Get Started, Click on the current week below.</Header>
+        <Grid columns={2}>
+          <Grid.Row>
+            <Grid.Column>
+              <Card.Content style={{backgroundImage: 'url(https://www.lds.org/bc/content/bible-videos/videos/mary-and-elizabeth-rejoice-together/images/mary-visits-elisabeth.jpg)', backgroundRepeat: 'no-repeat', backgroundSize: 'contain', marginLeft: '8vw', marginTop: '5vh'}}>
+                <div style={{height: '25vh', width: 'auto', color: 'white', fontSize: '16px'}}>Week 1-ish...</div>
+              </Card.Content>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
