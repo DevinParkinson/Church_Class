d450fe8a8ba800b133b4c952534efa74a223834f
Devin Parkinson <dparky13@icloud.com>
2019-01-14 23:05:14 -0700

    more of a mobile responsive home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,14 +8,14 @@ class Home extends Component {
     return (
       <div>
         <div style={{ backgroundImage: "url(https://www.followmeretreat.org/wp-content/uploads/2017/03/cropped-Jesus-Hand-Vanilla-2.jpg)", backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: "100vh", backgroundColor: "white", backgroundPosition: "center", position: "relative" }}>
-          <div style={{textAlign: "center", position: "absolute", top: "25%", left: "20%", color: "white"}}>
+          <div style={{textAlign: "center", position: "absolute", top: "25%", left: "10%", color: "white"}}>
             <TextLoop>
               <Header as='h1' style={{color: "white"}}>Hello 17 year olds!</Header>
               <Header as='h1' style={{color: "white"}}>Welcome to your own website for this year!</Header>
               <Header as='h1' style={{color: "white"}}>Let's Rock this!!!</Header>
             </TextLoop>
           </div>
-          <Header as='h2' style={{textAlign: "center", position: "absolute", top: "30%", left: "15%", color: "white"}}>To Get Started, Click on the current week below.</Header>
+          <Header as='h2' style={{textAlign: "center", position: "absolute", top: "30%", left: "8%", color: "white"}}>To Get Started, Click on the current week below.</Header>
         </div>
         <Grid columns={2}>
           <Grid.Row>
